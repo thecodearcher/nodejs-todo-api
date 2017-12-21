812d84117db745a0f9b00d29483e461de04b9a28
Brian Iyoha <brian98ehis@gmail.com>
2017-12-21 11:50:09 +0100

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -53,6 +53,26 @@ app.get('/todos/:id',(req,res)=>{
     }).catch(e=>res.status(400).send());
 });
 
+app.delete('/todos/:id',(req,res)=>{
+
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404)
+        .send();
+    }
+    
+    Todo.findByIdAndRemove(id)
+    .then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch(e=>res.status(400).send());
+});
+
 app.listen(port,()=>{
     console.log('Server Started On Port ' + port);
 });
