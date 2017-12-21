d4d7237abdc71ac0786c24067c8c43db9a70566c
Brian Iyoha <brian98ehis@gmail.com>
2017-12-21 10:53:22 +0100

    Added /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const { ObjectID } = require('mongodb'); 
+
 const express =require('express');
 const bodyParser = require('body-parser');
 
@@ -20,7 +22,7 @@ app.post('/todos',(req,res)=>{
         res.send(e);
     });
 });
-
+ju = '349822552';
 app.get('/todos',(req,res)=>{
     Todo.find({})
     .then((todos)=>{
@@ -28,6 +30,26 @@ app.get('/todos',(req,res)=>{
             todos
         });
     }).catch(e=>res.send(e));
+});
+
+app.get('/todos/:id',(req,res)=>{
+
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404)
+        .send();
+    }
+    
+    Todo.findById(id)
+    .then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch(e=>res.status(400).send());
 });
 
 app.listen(3000,()=>{
