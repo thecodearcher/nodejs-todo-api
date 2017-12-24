ffb8d4c864d649e9b21bb3bc28bce327bb238403
Brian Iyoha <brian98ehis@gmail.com>
2017-12-24 14:29:33 +0100

    Configured env variable

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 var env = process.env.NODE_ENV || "development";
 
 var envConfig = config[env];
-if(env=='development' || env==='test'){
-    var config  = require('./config.json');
-    Object.keys(envConfig).forEach((key)=>{
-        process.env[key] = envConfig[key];
-    });
-}
+// if(env=='development' || env==='test'){
+//     var config  = require('./config.json');
+//     Object.keys(envConfig).forEach((key)=>{
+//         process.env[key] = envConfig[key];
+//     });
+//}
